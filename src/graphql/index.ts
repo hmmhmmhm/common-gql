f3b2d243a7e7e9e9fc44fe4fec4f179ed50175f4
hmmhmmhm <hmmhmmhm@naver.com>
2019-05-24 20:27:51 +0900

    Added graphql index

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,3 +1,3 @@
 import typeDefs from './Typedefs'
 import resolvers from './Resolvers'
-export default { typeDefs, resolvers }
+export { typeDefs, resolvers }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ApolloClient, gql } from 'apollo-boost'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { SchemaLink } from 'apollo-link-schema'
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
-
+import { typeDefs, resolvers } from './graphql'
 
 const cache = new InMemoryCache()
 const executableSchema = makeExecutableSchema({typeDefs})
